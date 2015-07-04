d641b6e7d0af6615a96d3217e82370ccb8367853
Linnik85 <apple@Linnik-MacBook-Pro.local>
2015-07-04 15:20:43 +0300

    Initial Commit

---
+++ WeatherApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherApp
+//
+//  Created by Линник Александр on 04.07.15.
+//  Copyright (c) 2015 Alex Linnik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
